2baaa2a67fb7febb8dbeca7cd102a7f6f7e224c0
tanvigupta27 <62935426+tanvigupta27@users.noreply.github.com>
2022-10-18 15:27:37 +0530

    tanvigupta27: Added Assignment 4 (#14)
    Co-authored-by: Gupta <TG086294@cerner.net>

---
+++ tanvigupta27/A4_Q4.py
@@ -0,0 +1,20 @@
+A = ['a','aa','aab','b','bb','bba','bbb']
+B = 'bb'
+C = ['a','b']
+D = 'c'
+
+def prefix_matching(A,B):
+    
+    res = []
+    for index, i in enumerate(A):
+        if B in i:
+            res.append(index)
+    if(res):
+        print(res[::len(res)-1])
+    else:
+        print("[-1,-1")
+    return
+print("Output 1:")
+prefix_matching(A,B)
+print("Output 2:")
+prefix_matching(C,D)
